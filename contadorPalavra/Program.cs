d113981a6320b6ac855cf79bcfba4bbf70f7b425
Vinicius Silva Lima <silvalimavinicius515@gmail.com>
2025-04-10 10:52:43 -0300

    Finalizando o projeto Contador de Palavras com contagem de palavras e caracteres.

---
+++ contadorPalavra/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Seja bem-vindo ao contador de palavras!");
+
+Console.WriteLine("Digite o texto que deseja analisar:");
+string texto = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(texto))
+{
+    Console.WriteLine("Texto inválido.");
+}
+else
+{
+    string[] palavras = texto.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+    int numeroPalavras = palavras.Length;
+    int caracteresComEspacos = texto.Length;
+    int caracteresSemEspacos = texto.Count(c => !char.IsWhiteSpace(c));
+
+    Console.WriteLine($"Palavras: {numeroPalavras}");
+    Console.WriteLine($"Caracteres: {caracteresSemEspacos}");
+}
+Console.WriteLine("Obrigado por usar o contador de palavras!");
